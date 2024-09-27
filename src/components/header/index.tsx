67060d3cbf47edf2a36a270ef6910db766ec1468
ramonabreu <ramon.vitor@etipi.pi.gov.br>
2024-09-27 13:01:04 -0300

    csss

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link";
 import styles from "./header.module.css"
+import { signIn, signOut, useSession } from "next-auth/react";
+import Google from "next-auth/providers/google";
 export default function Header(){
+    const {data:session , status} = useSession();
     return(
         <header className={styles.header}> 
             <section className={styles.content}>
                 <nav className={styles.nav}>
+                    <Link href="/">
                     <h1 className={styles.tarefas}>tarefas <span className={styles.span}>+</span></h1>
-                    <Link href="/dashboard/Dashboard"> meu painel</Link>
+                    </Link>
+                    
+                    <Link href="/dashboard/Dashboard" className={styles.painel}> meu painel</Link>
                 </nav>
-                <button className={styles.button}>cadastre-se</button>
+                {status === "loading" ?(
+                    <></>
+                ): session?(
+                    <button className={styles.button} onClick={()=> signOut()}>olá {session?.user?.name}</button>
+                ): (
+                    <button className={styles.button} onClick={()=>signIn("google")}>cadastre-se</button>
+                )}
             </section>
         </header>
     )
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import Header from "@/components/header";
 import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
+  <SessionProvider session={pageProps.session}>
   <Header/>
   <Component {...pageProps} />;
-  </>)
+  </SessionProvider>)
 }
