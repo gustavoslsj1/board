a7f7b5d1bc68bb9bd36b033df8f1afd9255d866f
gustavo <gustavo6555555@gmail.com>
2024-10-02 11:31:44 -0300

    terminando o site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css"
 import hero from "../../public/hero.png"
+import { GetServerSideProps } from "next";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/services/firebaseconnection";
 
-export default function Home(){
+interface HomeProps{
+  coment: number,
+  post:number,
+}
+
+export default function Home({coment, post}:HomeProps){
   return(
     <div className={styles.container}>
       <Head><title>tarefas+| organize suas tarefas de forma facil</title></Head>
@@ -19,10 +27,26 @@ export default function Home(){
         </div>
      
         <div className={styles.caixa}>
-        <p className={styles.caixasFrase}> +1 mil posts</p>
-        <p className={styles.caixasFrase}> +7 mil comentarios </p>
+        <p className={styles.caixasFrase}> +{post} mil posts</p>
+        <p className={styles.caixasFrase}> +{coment} mil comentarios </p>
       </div>
       </main>
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async()=>{
+  
+  const comentsRef = collection(db, "coments")
+  const postRef = collection(db, "tarefas")
+
+  const comentsSnapshot = await getDocs(comentsRef)
+  const postSnapshot = await getDocs(postRef)
+  return{
+    props:{
+      coment: comentsSnapshot.size || 0,
+      post: postSnapshot.size || 0
+    }
+  }
+
 }
--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -2,8 +2,12 @@ import Head from "next/head";
 import styles from "./task.module.css"
 import { GetServerSideProps } from "next";
 import { db } from "@/services/firebaseconnection";
-import {doc, collection, query, getDoc} from "firebase/firestore"
+import {doc, collection, query, getDoc, addDoc, where, getDocs, deleteDoc} from "firebase/firestore"
+import TextBoard from "@/components/textboard";
+import { useSession } from "next-auth/react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
+import { FaTrash } from "react-icons/fa";
 interface TaskProps{
     item:{
         tarefas:string,
@@ -11,19 +15,111 @@ interface TaskProps{
         created:string,
         user:string,
         taskId:string
-    }
+    },
+    allcoments:ComentsProps[]
 }
 
-export default function Task({item}:TaskProps){
+interface ComentsProps{
+      comentario:string,
+      id: string,
+      name:string,
+      taskId:string,
+      user:string 
+}
+
+export default function Task({item, allcoments}:TaskProps){
+
+    const{data:Session}= useSession()
+    const[input, setinput]= useState("")
+
+    const[comentario, setcomentario] = useState<ComentsProps[]>(allcoments || [])
+    
+    async function handlecoment(event: FormEvent){
+        event.preventDefault();0
+
+        if(input === "") return;
+
+        if(!Session?.user?.name || !Session?.user.email) return
+
+        try{
+            const docRef = await addDoc(collection(db,"coments"),{
+                comentario: input,
+                created:new Date(),
+                user: Session?.user?.email,
+                name: Session?.user?.name,
+                taskId: item.taskId
+            })
+
+            const Data={
+                id: docRef.id,
+                comentario: input,
+                user: Session?.user?.email,
+                name: Session?.user?.name,
+                taskId: item?.taskId
+            }
+
+            setcomentario((oldcomentario)=>[...oldcomentario , Data])
+            setinput('')
+        }catch{
+            console.log('')
+        }
+
+
+    }
+    async function handleDelet(id:string){
+        try{
+            const docRef = doc(db,"coments", id)
+            await deleteDoc(docRef)
+            const deletComent = comentario.filter((item)=> item.id !== id )
+            setcomentario(deletComent)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return(
         <div className={styles.container}>
-            <Head> <title>{item.tarefas}</title></Head>
+            <Head> <title>comentarios tarefas</title></Head>
 
             <main className={styles.main}>
                 <h1>tarefa</h1>
              <article className={styles.article}>
                 <p className={styles.tarefa}>{item.tarefas}</p>
              </article>
+
+            <section className={styles.divcomentarios}>
+                <h3>deixar comentario</h3>
+                <form onSubmit={handlecoment}>
+                    <TextBoard placeholder="digite seu comentario"
+                    value={input}
+                    onChange={(event: ChangeEvent<HTMLTextAreaElement>)=> setinput(event.target.value)}/>
+                    <button className={styles.button}
+                    disabled={!Session?.user}>enviar comentario</button>
+                </form>
+            </section>
+
+            <section className={styles.sectionComents}>
+                <h2>comentarios</h2>
+                {comentario.length === 0 &&(
+                    <span className={styles.span}>sem comentarios </span>
+                )}
+
+                {comentario.map((item)=>(
+                    <article key={item.id} className={styles.articlecoments}>
+                        <div className={styles.headerComents}>
+                            <label className={styles.label} >{item.name}</label>
+                          {item.user === Session?.user?.email &&(
+                            <button className={styles.trash} onClick={()=>handleDelet(item.id)}>
+                              <FaTrash size={18} color="red" />
+                            </button>
+                          )}
+                        </div>
+                        <p className={styles.usercoments}>{item.comentario}</p>
+                    </article>
+                ))}
+            </section>
+            
+            
             </main>
         </div>
     )
@@ -33,6 +129,24 @@ export const getServerSideProps: GetServerSideProps = async({params}) =>{
     const id = params?.id as string;
 
     const docRef = doc(db,"tarefas",id)
+
+    const q = query(collection(db,"coments"), where("taskId", "==", id))
+
+    const snapshotComments = await getDocs(q);
+
+    let allcoments:ComentsProps[] =[]
+
+    snapshotComments.forEach((doc) => {
+        allcoments.push({
+            id: doc.id,
+            comentario: doc.data().comentario,
+            user: doc.data().user,
+            name: doc.data().name,
+            taskId: doc.data().taskId,
+        })
+    });
+    console.log(allcoments)
+
     const snapshot = await getDoc(docRef)
 
     if(snapshot == undefined){
@@ -67,6 +181,7 @@ export const getServerSideProps: GetServerSideProps = async({params}) =>{
     return{
         props:{
             item: task,
+            allcoments: allcoments,
         },
     }
 } 
